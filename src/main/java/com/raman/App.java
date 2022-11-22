e3286495edb5a54fbf578c8d69c0f0fd7ea6b200
nibin235 <86356013+nibin235@users.noreply.github.com>
2022-11-22 11:12:32 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "New Automation Webhook - Poll Scm Testing -124456" );
+        System.out.println( "Maven Testing" );
     }
 }
